3c3d47e78a1bae8f8d218fd3c43d7040ca6171b2
h-rsh-n <harshanp1514@gmail.com>
2024-06-13 17:56:38 +0530

    Added bullBoard

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
 import sampleQueueProcuder from "./producers/sampleQueueProcuder";
 import sampleWorker from "./workers/sampleQueueWorker";
+import serverAdapter from "./config/bullBoardConfig";
 
 const app = express();
 
 
-app.use('/api',apiRouter)
+app.use('/api',apiRouter);
+app.use('/ui',serverAdapter.getRouter());
 
 app.listen(serverConfig.PORT,()=>{
-  console.log(`server running on port 3000`);
+  console.log(`server running on port ${serverConfig.PORT}`);
+  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
 
   sampleWorker('SampleQueue');
 
@@ -20,6 +23,12 @@ app.listen(serverConfig.PORT,()=>{
     company:"MicroSoft",
     location:"Bengaluru"
   });
+
+  sampleQueueProcuder('SampleJob',{
+    name:"Pragya Das",
+    company:"GoldmanSachs",
+    location:"Hyderabad"
+  })
 })
 
 
